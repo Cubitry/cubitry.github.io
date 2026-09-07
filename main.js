17c31b2852ef3bbca051e8bf5b9017804ad59194
Scratchy2 <152662914+Scratchy2@users.noreply.github.com>
2026-09-07 11:36:09 +0500

    Enhance timer to show elapsed time on stop
    Added functionality to display elapsed time when stopping the timer.

--- main.js
+++ main.js
@@ -17,5 +17,6 @@ document.querySelector("#start").onclick = () => {
     } else {
         document.querySelector("#start").textContent = "Start";
         clearInterval(interval);
+        document.querySelector("#seconds").textContent = ((Date.now()-start)/1000).toFixed(2);
     }
 }
