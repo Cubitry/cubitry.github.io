4b2cceac02425b2e86f2a477adf3f0448b5789a2
Scratchy2 <152662914+Scratchy2@users.noreply.github.com>
2026-09-07 21:55:41 +0500

    Change timer to display minutes and seconds
    Updated timer display format to show minutes and seconds.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { randomScrambleForEvent as scramble } from "https://cdn.cubing.net/v0/js/cubing/scramble";
 function timer() {
-    document.querySelector("#seconds").textContent = ((Date.now()-start)/1000).toFixed(2);
+    document.querySelector("#seconds").value = `${Math.floor((Date.now()-start)/60000)}:${((Date.now()-start)/1000 % 60).toFixed(2)}`;
 }
 let start = 0; let interval;
 window.onload = async () => {
@@ -17,7 +17,7 @@ document.querySelector("#start").onclick = async () => {
     } else {
         document.querySelector("#start").textContent = "Start";
         clearInterval(interval);
-        document.querySelector("#seconds").textContent = ((Date.now()-start)/1000).toFixed(2);
+        document.querySelector("#seconds").value = ((Date.now()-start)/1000).toFixed(2);
         document.querySelector("#scramble").textContent = await scramble(document.querySelector("#scrtype").value);
     }
 }
