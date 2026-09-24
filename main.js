62615d0d1080fc28ce7bb918f653673f15fc49ed
Scratchy2 <152662914+Scratchy2@users.noreply.github.com>
2026-09-24 22:34:15 +0500

    Refactor timer functions and event handlers

--- main.js
+++ main.js
@@ -25,10 +25,11 @@ if (localStorage.getItem("times")) times = localStorage.getItem("times").split("
 window.onload = async () => {
     document.querySelector("#scramble").textContent = await scramble("333");
     for (let time in times) {
-        let t = times[time];
+        let i = Number(time);
+        let t = times[i];
         let mo3 = "---";
         if (time <= times.length - 2) {
-            mo3 = [t, times[time+1], times[time+2]];
+            mo3 = [t, times[i+1], times[i+2]];
             mo3 = mo3.includes("DNF") ? "DNF" : mo3.reduce((a, c) => a + Number(c)/3, 0);
             if (mo3 != "DNF") {
                 if (mo3 < 60) {mo3 = `${mo3.toFixed(2)}`;}
@@ -37,7 +38,7 @@ window.onload = async () => {
         }
         let ao5 = "---";
         if (time <= times.length - 4) {
-            ao5 = [t, times[time+1], times[time+2], times[time+3], times[time+4]].sort((a,b)=>a-b).slice(1,4);
+            ao5 = [t, times[i+1], times[i+2], times[i+3], times[i+4]].sort((a,b)=>a-b).slice(1,4);
             ao5 = ao5.filter(ti => ti == "DNF").length >= 2 ? "DNF" : ao5.reduce((a, c) => a + Number(c)/3, 0);
             if (ao5 != "DNF") {
                 if (ao5 < 60) {ao5 = `${ao5.toFixed(2)}`;}
@@ -46,7 +47,7 @@ window.onload = async () => {
         }
         let ao12 = "---";
         if (time <= times.length - 11) {
-            ao12 = [t, times[time+1], times[time+2], times[time+3], times[time+4], times[time+5], times[time+6], times[time+7], times[time+8], times[time+9], times[time+10], times[time+11]].sort((a,b)=>a-b).slice(1,11);
+            ao12 = [t, times[i+1], times[i+2], times[i+3], times[i+4], times[i+5], times[i+6], times[i+7], times[i+8], times[i+9], times[i+10], times[i+11]].sort((a,b)=>a-b).slice(1,11);
             ao12 = ao12.filter(ti => ti == "DNF").length >= 2 ? "DNF" : ao12.reduce((a, c) => a + Number(c)/10, 0);
             if (ao12 != "DNF") {
                 if (ao12 < 60) {ao12 = `${ao12.toFixed(2)}`;}
